2a6cc58ff5c45806a3f10966eb09283dbdb2b212
Mark Vodyanitskiy <mvodya@gmail.com>
2017-12-19 17:18:23 +1000

    Gameloop with tick

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,12 +1,23 @@
 ﻿#include <iostream>
+#include <chrono>
+#include <thread>
 #include "snake.h"
+
+Snake *snake;
 
 // Основной игровой цикл
 void gameLoop() {
-
 }
 
 int main() {
+	snake = new Snake();
+
+	while (true)
+	{
+		gameLoop(); // Запускаем игровой цикл
+		std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Задержка (тик цикла)
+	}
+
 	system("pause");
 	return 0;
 }
