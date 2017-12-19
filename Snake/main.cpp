785a9365e09af3ada8f0a79556efaab30a1358a1
Mark Vodyanitskiy <mvodya@gmail.com>
2017-12-19 16:43:06 +1000

    Основа

---
+++ Snake/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "snake.h"
+
+// Основной игровой цикл
+void gameLoop() {
+
+}
+
+int main() {
+	system("pause");
+	return 0;
+}
---
+++ Snake/snake.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <deque>
+
+// Кусок хвоста
+struct Sector { int x, y; };
+
+class Snake {
+private:
+	int _direction; // Направление движения
+	std::deque<Sector> _sectors; // Стек секторов хвоста
+public:
+};
