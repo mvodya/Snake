289a8f4397ca5e16c1ec302603d85fabb3fc053a
Mark Vodyanitskiy <mvodya@gmail.com>
2017-12-23 20:04:55 +1000

    Controller comments

--- Snake/controller.cpp
+++ Snake/controller.cpp
@@ -2,27 +2,33 @@
 
 // Управление направлениtv змейки с клавиатуры
 void Controller::controlDirection(Snake *snake) {
+	// Налево
 	if (GetKeyState('A') & 0x8000)
 	{
 		snake->setDirection(3);
 	}
+	// Направо
 	if (GetKeyState('D') & 0x8000)
 	{
 		snake->setDirection(1);
 	}
+	// Вверх
 	if (GetKeyState('W') & 0x8000)
 	{
 		snake->setDirection(2);
 	}
+	// Ввниз
 	if (GetKeyState('S') & 0x8000)
 	{
 		snake->setDirection(4);
 	}
 }
 
+// Отрисовка граней карты
 void Controller::drawBorder() {
 	COORD Coord;
 
+	// Верх
 	for (size_t i = 0; i < SIZE_BORDER_X; i++)
 	{
 		Coord.X = i;
@@ -30,6 +36,7 @@ void Controller::drawBorder() {
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Coord);
 		std::cout << "-";
 	}
+	// Низ
 	for (size_t i = 0; i < SIZE_BORDER_X; i++)
 	{
 		Coord.X = i;
@@ -37,6 +44,7 @@ void Controller::drawBorder() {
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Coord);
 		std::cout << "-";
 	}
+	// Левая грань
 	for (size_t i = 1; i < SIZE_BORDER_Y; i++)
 	{
 		Coord.X = 0;
@@ -44,6 +52,7 @@ void Controller::drawBorder() {
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Coord);
 		std::cout << "|";
 	}
+	// Правая грань
 	for (size_t i = 1; i < SIZE_BORDER_Y; i++)
 	{
 		Coord.X = SIZE_BORDER_X - 1;
